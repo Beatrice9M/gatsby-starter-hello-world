efbf6720e7171c099768c673dec3fc955260fe07
Beatrice.Gojae <beatrice.goja@e-mundo.dem>
2022-04-21 14:38:48 +0200

    do it

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,22 @@ import React from "react"
 export default function Home() {
   return (
     <div className="contactForm">
-      <form name="contactForm" method="post" data-netlify="true">
+      <p className="heading" style={{ color: "blue", textAlign: "center" }}>
+        My Contact Form
+      </p>
+      <form
+        name="contactForm"
+        method="post"
+        data-netlify="true"
+        style={{ textAlign: "center" }}
+      >
         <input type="hidden" name="contactForm" value="contactForm"></input>
         <input type="text" placeholder="enter name" name="name"></input>
         <input type="text" placeholder="enter email" name="email"></input>
         <textarea placeholder="enter message" name="message"></textarea>
-        <button type="submit">Submit Form</button>
+        <button type="submit" style={{ color: "blue", textAlign: "center" }}>
+          Submit Form
+        </button>
       </form>
     </div>
   )
---
+++ src/pages/succesPage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div className="sucsessPage">
+      <div>Thank you for your message</div>
+    </div>
+  )
+}
