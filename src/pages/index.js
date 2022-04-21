ecbc3dbc8252ea502d1fcf0544af542e94293b4f
Beatrice.Gojae <beatrice.goja@e-mundo.dem>
2022-04-21 11:26:30 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="contactForm">
+      <form name="contactForm" method="post" data-netlify="true">
+        <input type="hidden" name="contactForm" value="contactForm"></input>
+        <input type="text" placeholder="enter name" name="name"></input>
+        <input type="text" placeholder="enter email" name="email"></input>
+        <textarea placeholder="enter message" name="message"></textarea>
+        <button type="submit"></button>
+      </form>
+    </div>
+  )
 }
