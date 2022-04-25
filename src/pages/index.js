3b181a8993e7134a3d27939246e8f20a1f13f7e0
Beatrice Goja <beatrice.goja@e-mundo.de>
2022-04-25 18:16:14 +0200

    fix: convert to react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,19 @@
-import React from "react"
+import React, { useCallback, useState } from "react"
 
 export default function Home() {
-  function changeFormName(event) {
-    // The value of the subject field.
-    const value = event.target.value
-    // The name we want to apply to the form, based on the value.
-    const name = "Contact-" + value
-    // The form element in the DOM.
-    const form = document.querySelector("#conditional-form")
-    // Apply the new name to the form's [name] attribute.
-    if (form) {
-      form.setAttribute("name", name)
-    }
-    console.log("name", name)
-    // The [name="form-name] field in the DOM.
-    const formName = document.querySelector(
-      '#conditional-form [name="Contact-Marketing"]'
-    )
-    // Apply the new name to the [name="form-name"] field within the form.
-    if (formName) {
-      formName.setAttribute("value", name)
-    }
-  }
+  const [formName, setFormName] = useState("Contact-Marketing")
+
+  const changeFormName = useCallback(
+    event => {
+      // The value of the subject field.
+      const value = event.target.value
+      // The name we want to apply to the form, based on the value.
+      const name = "Contact-" + value
+      setFormName(name)
+    },
+    [setFormName]
+  )
+
   return (
     <div className="contactForm">
       <p className="heading" style={{ color: "blue", textAlign: "center" }}>
@@ -29,20 +21,15 @@ export default function Home() {
       </p>
       <form
         id="conditional-form"
-        name="Contact-Marketing"
         method="POST"
+        name={formName}
         data-netlify="true"
         style={{ textAlign: "center" }}
         action="/success/"
       >
         <input type="text" placeholder="enter name" name="name"></input>
         <input type="text" placeholder="enter email" name="email"></input>
-        <input
-          type="text"
-          placeholder="enter something"
-          name="something"
-        ></input>
-        <select name="subject" required onchange="changeFormName(event)">
+        <select name="subject" required onChange={ev => changeFormName(ev)}>
           <option value="Marketing">Marketing</option>
           <option value="Sales">Sales</option>
           <option value="Operations">Operations</option>
